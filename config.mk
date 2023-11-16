5279d570611f2763e0ed41a3a1aab50227035090
Miguel Perez Andrade <miguel.perezandrade@gmail.com>
2023-11-16 19:46:40 -0300

    restored installation paths to /usr/local

--- config.mk
+++ config.mk
@@ -4,11 +4,11 @@ VERSION = 0.9
 # Customize below to fit your system
 
 # paths
-PREFIX = /home/mperezandrade/compiled
+PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /home/mperezandrade/Downloads/usr/include/X11
-X11LIB = /usr/lib64
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
 
 PKG_CONFIG = pkg-config
 
@@ -30,6 +30,7 @@ STLDFLAGS = $(LIBS) $(LDFLAGS)
 #LIBS = -L$(X11LIB) -lm -lX11 -lutil -lXft \
 #       `$(PKG_CONFIG) --libs fontconfig` \
 #       `$(PKG_CONFIG) --libs freetype2`
+#MANPREFIX = ${PREFIX}/man
 
 # compiler and linker
 # CC = c99
